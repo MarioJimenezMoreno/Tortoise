07875c10d84784168dacfdb20d7d2e268f6fe29c
Mario <mariojm2612@gmail.com>
2024-02-11 20:34:39 +0100

    updated

--- src/Components/MainPage.tsx
+++ src/Components/MainPage.tsx
@@ -1,21 +1,17 @@
 // import "../Styles/MainPage.css";
-import Header from './Header'
+import Header from "./Header";
 import Home from "./Main Page/Home";
 import Info from "./Main Page/Info";
-import Team from "./Main Page/Team";
 
 function MainPage() {
-
   return (
-    <> 
-      <Header/>
-      <div id="home"/>
-      <Home/>
-      <div id="info"/>
-      <Info/>
-      <div id="team"/>
-      <Team/>
+    <>
+      <Header />
+      <div id="home" />
+      <Home />
+      <div id="info" />
+      <Info />
     </>
-  )
+  );
 }
 export default MainPage;
